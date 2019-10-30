e5a272eb421faa36e8739fa6c2a2dc171a92988d
USP <USP@10.15.10.146>
2019-10-30 18:50:40 -0500

    completado el trabajo

--- index.php
+++ index.php
@@ -9,10 +9,13 @@
 <div style='margin: 15px;'>
     <span class='h2'>Figuras geometricas</span>
     <br>
-    <div style='margin-top: 10px;'>
+    <div style='margin: 10px 0;'>
         <a id='btnOctogono' href='octogono.php' class='btn btn-info'>Octogono</a>
         <a id='btnRomboide' href='romboide.php' class='btn btn-info'>Romboide</a>
         <a id='btnTrapecio' href='trapecio.php' class='btn btn-info'>Trapecio</a>
+    </div>
+    <div style='margin: 10px 0;'>
+        Calcular el área de las figuras geométricas
     </div>
 </div>
 </body>
