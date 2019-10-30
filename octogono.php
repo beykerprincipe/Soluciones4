36fce980e4958e307bda006c28cf985a1f5f3c78
USP <USP@10.15.10.146>
2019-10-30 18:07:20 -0500

    he programado el octogono

--- octogono.php
+++ octogono.php
@@ -4,19 +4,44 @@
 <!DOCTYPE html>
 <html lang='es'>
 <head>
-	<title>Figuras geometricas</title>
-	<link rel='stylesheet' href='css/bootstrap.css'>
-	<link rel='stylesheet' href='css/bootstrap-theme.css'>
+    <title>Figuras geometricas</title>
+    <link rel='stylesheet' href='css/bootstrap.css'>
+    <link rel='stylesheet' href='css/bootstrap-theme.css'>
 </head>
 <body>
-<div style='margin: 15px;'>
-	<span class='h2'>Octogono</span>
-	
-	
-	
-	<div style='margin-top: 10px;'>
-		<a id='btnOctogono' href='index.php' class='btn btn-danger'>Volver</a>
-	</div>
+<div style='padding: 15px;'>
+    <span class='h2'>Octogono</span>
+    <br>
+    <img src='img/octogono.png' alt='octógono'>
+    <div style='margin: 10px 0px; padding: 10px; background: #e7eff6'>
+        Área = 4 * L * ap
+    </div>
+    <form method='post'>
+        <div style='padding: 10px 5px;'>
+            <label for='txtLado'>Lado (L): </label>
+            <input type='text' id='txtLado' class='form-control' style='width: 300px;'> cm
+        </div>
+        <div style='padding: 10px 5px;'>
+            <label for='txtApotema'>Apotema (ap): </label>
+            <input type='text' id='txtApotema' class='form-control' style='width: 300px;'> cm
+        </div>
+        <div style='padding: 10px 5px;'>
+            <label for='txtArea'>Área: </label>
+            <input disabled type='text' id='txtArea' class='form-control' style='width: 300px;'> cm2
+        </div>
+        <div style='margin-top: 10px; text-align: center;'>
+            <a id='btnCalcular' href='#' class='btn btn-danger' onclick='calcular()'>Calcular</a>
+            <a id='btnVolver' href='index.php' class='btn btn-default'>Volver</a>
+        </div>
+    </form>
 </div>
+<script>
+    function calcular() {
+        let lado = document.getElementById('txtLado').value;
+        let apotema = document.getElementById('txtApotema').value;
+        let area = 4 * parseFloat(lado) * parseFloat(apotema);
+        document.getElementById('txtArea').value = area;
+    }
+</script>
 </body>
 </html>
