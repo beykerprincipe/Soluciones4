38e1bc93c6867b51c7d20c16fc27c22ef0d3866e
USP <USP@10.15.10.148>
2019-10-30 18:37:19 -0500

    calculado el area del romboide

--- romboide.php
+++ romboide.php
@@ -7,36 +7,37 @@
 </head>
 <body>
 <div style='padding: 15px;'>
-    <span class='h2'>Octogono</span>
+    <span class='h2'>Romboide</span>
     <br>
-    <img src='img/octogono.png' alt='octógono'>
+    <img src='img/romboide-area.jpg' alt='octógono'>
     <div style='margin: 10px 0px; padding: 10px; background: #e7eff6'>
-        Área = 4 * L * ap
+        Área = b x h
     </div>
     <form method='post'>
         <div style='padding: 10px 5px;'>
-            <label for='txtLado'>Lado (L): </label>
-            <input type='text' id='txtLado' class='form-control' style='width: 300px;'> cm
+            <label for='txtBase'>Base (b): </label>
+            <input type='text' id='txtBase' class='form-control' style='width: 300px;'> cm
         </div>
         <div style='padding: 10px 5px;'>
-            <label for='txtApotema'>Apotema (ap): </label>
-            <input type='text' id='txtApotema' class='form-control' style='width: 300px;'> cm
+            <label for='txtAltura'>Altura (h): </label>
+            <input type='text' id='txtAltura' class='form-control' style='width: 300px;'> cm
         </div>
         <div style='padding: 10px 5px;'>
             <label for='txtArea'>Área: </label>
             <input disabled type='text' id='txtArea' class='form-control' style='width: 300px;'> cm2
         </div>
         <div style='margin-top: 10px; text-align: center;'>
-            <a id='btnCalcular' href='#' class='btn btn-danger' onclick='calcular()'>Calcular</a>
+            <a id='btnCalcular' href='#' class='btn btn-danger' onclick='calcular()'>Calcular area</a>
             <a id='btnVolver' href='index.php' class='btn btn-default'>Volver</a>
         </div>
     </form>
 </div>
 <script>
     function calcular() {
-        let lado = document.getElementById('txtLado').value;
-        let apotema = document.getElementById('txtApotema').value;
-        let area = 4 * parseFloat(lado) * parseFloat(apotema);
+        let b    = document.getElementById('txtBase').value;
+        let h    = document.getElementById('txtAltura').value;
+        let area = parseFloat(b) * parseFloat(h);
+
         document.getElementById('txtArea').value = area;
     }
 </script>
