87e1779d9fbb2bf5fc6ee5ef93be7692f43375f5
USP <USP@10.15.10.147>
2019-10-30 16:12:35 -0500

    agregado el bootstrap

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+    <title>Figuras geometricas</title>
+    <link rel='stylesheet' href='css/bootstrap.css'>
+    <link rel='stylesheet' href='css/bootstrap-theme.css'>
+</head>
+<body>
+<div style='margin: 15px;'>
+    <span class='h2'>Figuras geometricas</span>
+    <br>
+    <div style='margin-top: 10px;'>
+        <a id='btnOctogono' class='btn btn-info'>Octogono</a>
+        <a id='btnRomboide' class='btn btn-info'>Romboide</a>
+        <a id='btnTrapecio' class='btn btn-info'>Trapecio</a>
+    </div>
+</div>
+</body>
+</html>
